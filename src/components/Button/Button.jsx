3f6b57cb920bd610db178b6ddbe4edc2b5995a3a
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-08-14 22:14:18 +0300

    fix

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-
+import { LoadButton } from './Button.styled'
 
 export default function Button({ onClick }) {
     return <LoadButton onClick={onClick}>Load more</LoadButton>;
