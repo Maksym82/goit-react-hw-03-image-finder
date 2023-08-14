55927f4daf4f08c0fc9a41f017a82adf3b9fce14
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-08-14 16:30:43 +0300

    fix

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,8 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
+import {
+    Header, SearchForm, SearchFormButton, SearchFormInput, StyleBiSearchAlt,
+} from './Searchbar.styled';
 
 
 
