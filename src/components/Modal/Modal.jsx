3b97ff4f8a871e2ddc32217fe7056ff911e91d32
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-08-15 15:48:07 +0300

    fix

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,43 +1,44 @@
 import PropTypes from 'prop-types';
 import { Component } from 'react';
 import { createPortal } from 'react-dom';
+
 import { ModalBackdrop, ModalStyles } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
 export default class Modal extends Component {
-    componentDidMount() {
-        window.addEventListener('keydown', this.handleKeyDown);
-    }
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
 
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleKeyDown);
-    }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
 
-    handleKeyDown = event => {
-        if(event.code === 'Escape') {
-            this.props.onClose();
-        }
+  handleKeyDown = evt => {
+    if (evt.code === 'Escape') {
+      this.props.onClose();
     }
+  };
 
-    onClickBackdrop = event => {
-        if(event.currentTarget === event.target) {
-            this.props.onClose();
-        }
+  onClickBackdrop = evt => {
+    if (evt.currentTarget === evt.target) {
+      this.props.onClose();
     }
+  };
 
-    render() {
-        return createPortal(
-          <ModalBackdrop onClick={this.onClickBackdrop}>
-            <ModalStyles>{this.props.children}</ModalStyles>
-          </ModalBackdrop>,
-          modalRoot
-        );
-    }
+  render() {
+    return createPortal(
+      <ModalBackdrop onClick={this.onClickBackdrop}>
+        <ModalStyles>{this.props.children}</ModalStyles>
+      </ModalBackdrop>,
+      modalRoot
+    );
+  }
 }
 
 Modal.propTypes = {
-    children: PropTypes.node.isRequired,
-    onClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
 
